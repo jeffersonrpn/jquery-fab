7062c37e84e8ffc871553e421aaca3b1cbead214
Daniel Lando <daniel.sorridi@gmail.com>
2018-05-23 16:16:38 +0200

    Added Tooltips, onclick and scale animation to menu buttons

--- dist/js/jquery-fab.js
+++ dist/js/jquery-fab.js
@@ -10,47 +10,52 @@
     if(!$.jfab){
         $.jfab = new Object();
     };
-    
-    $.jfab.fab = function(el, links, options){
-        var
-            base = this,
-            mainBtn,
-            subBtns;
-        
+
+    $.jfab.fab = function(el, buttons, options){
+        var base = this;
+
         // Access to jQuery and DOM versions of element
         base.$el = $(el);
         base.el = el;
         base.$el.data("kc.fab", base);
         base.$el.addClass("jfab_wrapper");
-        var toogleAnimation = function(e){
-            if ($(this).attr('data-link-href').length > 0){
-                if ($(this).attr('data-link-target')){
-                    window.open($(this).attr('data-link-href'), $(this).attr('data-link-target'));
-                }else{
-                    window.location.href = $(this).attr('data-link-href');
-                }
-            }
-            subBtns.animate({
-                opacity: "toggle",
-                height: "toggle"
-            }, 200);
+
+        var toogleAnimation = function(){
+            base.subBtns.toggleClass('scale-out');
+            base.$el.find('.sub_fab_title').toggle();
             if (options.rotate) {
-                mainBtn.toggleClass('rotate');
+                base.mainBtn.toggleClass('rotate');
             }
         };
         var hide = function(e){
-            subBtns.animate({
-                opacity: "hide",
-                height: "hide"
-            }, 100);
+            base.subBtns.addClass('scale-out');
+            base.$el.find('.sub_fab_title').hide();
+
             if (options.rotate) {
-                mainBtn.removeClass('rotate');
+                base.mainBtn.removeClass('rotate');
+            }
+        };
+        var doAction = function(index){
+            if(index == 0)
+                toogleAnimation();
+
+            var button = buttons[index];
+            if(button){
+                if (button.href){
+                    if (button.target){
+                        window.open(button.href, button.target);
+                    }else{
+                        window.location.href = button.href;
+                    }
+                }else if(button.onclick instanceof Function){
+                    button.onclick();
+                }
             }
         };
 
         base.init = function(){
-            if (typeof( links ) === "undefined" || links === null){
-                links = [
+            if (typeof( buttons ) === "undefined" || buttons === null){
+                buttons = [
                     {
                         "url":null,
                         "bgcolor":"#e74c3c",
@@ -64,24 +69,41 @@
                 };
             }
             base.options = $.extend({},$.jfab.fab.defaultOptions, options);
-            base.links = links;
-            if (base.links.length > 0){
-                mainBtnLink = base.links[0];
-                colorStyle = (mainBtnLink.color)? "color:"+mainBtnLink.color+";" : "";
-                bgColorStyle = (mainBtnLink.bgcolor)? "background-color:"+mainBtnLink.bgcolor+";" : "";
-                mainBtnLinkHtml = "<button data-link-href='"+((mainBtnLink.url)?mainBtnLink.url:"")+"' data-link-target='"+((mainBtnLink.target)?mainBtnLink.target:"")+"'' class='jfab_main_btn' style='"+bgColorStyle+"'><span style='"+colorStyle+"'>"+mainBtnLink.icon+"</span></button>";
-                
-                subBtnsHtml = "";
-                base.links.shift();
-                /* Loop through the remaining links array */
-                for (var i = 0; i < base.links.length; i++) {
-                    colorStyle = (base.links[i].color)? "color:"+base.links[i].color+";" : "";
-                    bgColorStyle = (base.links[i].bgcolor)? "background-color:"+base.links[i].bgcolor+";" : "";
-                    subBtnsHtml += "<div><button data-link-href='"+(base.links[i].url?base.links[i].url:"")+"' data-link-target='"+((base.links[i].target)?base.links[i].target:"")+"' class='sub_fab_btn' style='"+bgColorStyle+"'><span style='"+colorStyle+"'>"+base.links[i].icon+"</span></button></div>";
-                    
+            base.buttons = buttons;
+            if (base.buttons.length > 0){
+
+                var subBtns = $("<div class='jfab_btns_wrapper'></div>");
+
+                /* Loop through the remaining buttons array */
+                for (var i = 0; i < base.buttons.length; i++) {
+                    var colorStyle = (base.buttons[i].color)? "color:"+base.buttons[i].color+";" : "";
+                    var bgColorStyle = (base.buttons[i].bgcolor)? "background-color:"+base.buttons[i].bgcolor+";" : "";
+                    var button = $("<button style='"+bgColorStyle+"'><span style='"+colorStyle+"'>"+base.buttons[i].icon+"</span></button>");
+
+                    button.data('index', i);
+
+                    button.click(function(e){
+                        doAction($(this).data('index'));
+                    });
+
+                    if(i==0){
+                        button.addClass('jfab_main_btn');
+                        button.focusout(hide);
+                        base.$el.append(button);
+                        base.mainBtn = button;
+                    }else{
+                        button.addClass('sub_fab_btn scale-transition scale-out');
+                        button = $('<div class="sub_fab"></div>').append(button);
+
+                        if(base.buttons[i].title)
+                            button.prepend('<span class="sub_fab_title">'+base.buttons[i].title+'</span>');
+
+                        subBtns.prepend(button);
+                    }
                 }
-                subBtnsHtml = "<div class='jfab_btns_wrapper'>"+subBtnsHtml+"</div>";
-                base.$el.append(subBtnsHtml).append(mainBtnLinkHtml);
+
+                base.$el.prepend(subBtns);
+                base.subBtns = base.$el.find('.sub_fab_btn');
 
             }else{
                 if (typeof console == "undefined") {
@@ -91,33 +113,20 @@
                         }
                     };
                 }
-                console.log("Invalid links array param");
+                console.log("Invalid buttons array param");
             }
-            mainBtn = base.$el.find(".jfab_main_btn");
-            subBtns = base.$el.find(".jfab_btns_wrapper");
 
-            mainBtn.click(toogleAnimation);
-            mainBtn.focusout(hide);
-
-            subBtns.find('.sub_fab_btn').on('mousedown', function(e){
-                if ($(this).attr('data-link-href').length > 0){
-                    if ($(this).attr('data-link-target')){
-                        window.open($(this).attr('data-link-href'), $(this).attr('data-link-target'));
-                    }else{
-                        window.location.href = $(this).attr('data-link-href');
-                    }
-                }
-            });
         };
+
         base.init();
     };
-    
+
     $.jfab.fab.defaultOptions = {};
-    
-    $.fn.jqueryFab = function(links, options){
+
+    $.fn.jqueryFab = function(buttons, options){
         return this.each(function(){
-            (new $.jfab.fab(this, links, options));
+            (new $.jfab.fab(this, buttons, options));
         });
     };
-    
+
 })(jQuery);
